311226cdfa27e88157f93fd9982d1cccbb5bf0fd
ash hashtag <hashtag438@gmail.com>
2022-02-26 17:53:24 +0530

    upd

--- src/Application.cpp
+++ src/Application.cpp
@@ -59,7 +59,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "BitSim", NULL, NULL);
+    window = glfwCreateWindow(1280, 720, "BitSim", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
