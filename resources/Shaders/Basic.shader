44fbb35d8ee37f8a38fdcff57f31eef77b027e10
ash hashtag <hashtag438@gmail.com>
2022-02-27 19:35:23 +0530

    UPdate

--- resources/Shaders/Basic.shader
+++ resources/Shaders/Basic.shader
@@ -7,10 +7,12 @@ void main()
 };
 
 
+
 #shader Fragment
 #version 330 core
 layout(location = 0) out vec4 color;
+uniform vec4 u_color;
 void main()
 {
- color = vec4(1.0, 1.0, 0.0, 1.0);
+ color = u_color;
 };
---
+++ src/IndexBuffer.h
@@ -0,0 +1,19 @@
+#pragma once
+
+class IndexBuffer
+{
+private:
+	unsigned int m_RendererID;
+	unsigned int m_count;
+
+public:
+	IndexBuffer(const unsigned int* data, unsigned int count);
+	~IndexBuffer();
+
+	void Bind() const;
+	void Unbind() const;
+
+	inline unsigned int GetCount() const { return m_count; }
+};
+
+
