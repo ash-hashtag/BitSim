1fa293a7062ed954f4068ff83dbc2f18de594e40
ash hashtag <hashtag438@gmail.com>
2022-03-19 21:15:29 +0530

    imgui

--- src/Application.cpp
+++ src/Application.cpp
@@ -6,12 +6,19 @@
 #include <chrono>
 #include <fstream>
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
+
+
 #include "Renderer.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
 #include "Texture.h"
+#include "VertexBufferLayout.h"
 
 struct Timer {
     std::chrono::time_point<std::chrono::steady_clock> start, end;
@@ -22,30 +29,27 @@ struct Timer {
     ~Timer()
     {
         end = std::chrono::high_resolution_clock::now();
-        float ms = (end - start).count() * 1000.0f;
-        std::cout << "Timer took " << ms << "ms" << std::endl;
-
+        float ms = ((end - start).count());
+        std::cout << "MS: " << ms <<std::endl;
     }
 };
+
 
 
 int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(1280, 720, "BitSim", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "BitSim", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
@@ -57,13 +61,28 @@ int main(void)
     if (glewInit() != GLEW_OK)
         std::cout << "glew isn't opening\n";
     {
-
+        /*
         const float positions[16] = {
             -0.5f, -0.5f, 0.0f, 0.0f,
             0.5f, -0.5f, 1.0f, 0.0f,
             0.5f, 0.5f, 1.0f, 1.0f,
             -0.5f, 0.5f, 0.0f, 1.0f
+        }; */
+        
+        const float positions[16] = {
+            -50.0f, -50.0f, 0.0f, 0.0f,
+            50.0f, -50.0f, 1.0f, 0.0f,
+            50.0f, 50.0f, 1.0f, 1.0f,
+            -50.0f, 50.0f, 0.0f, 1.0f
         };
+        /*
+        const float positions[16] = {
+            1.0f, 1.0f, 0.0f, 0.0f,
+            2.0f, 1.0f, 1.0f, 0.0f,
+            2.0f, 2.0f, 1.0f, 1.0f,
+            1.0f, 2.0f, 1.0f, 1.0f
+        };*/
+
 
         const unsigned int indices[] = {
             0, 1, 2,
@@ -83,12 +102,18 @@ int main(void)
 
         IndexBuffer ib(indices, 6);
 
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+        glm::vec3 translationA(200, 200, 0);
+        glm::vec3 translationB(400, 200, 0);
+
         Shader shader("resources/shaders/Basic.shader");
         shader.Bind();
-        shader.SetUniform4f("u_color", 0.0f, 0.3f, 0.8f, 1.0f);
-        
-        Texture texture("resources/textures/cins.png");
-        texture.Bind();
+        shader.SetUniform4f("u_color", 0.0f, 0.3f, 0.8f, 0.1f);
+
+
+        Texture texture("resources/textures/rash.png");
+        texture.Bind(0);
         shader.SetUniform1i("u_Texture", 0);
 
         shader.Unbind();
@@ -96,29 +121,52 @@ int main(void)
         vb.Unbind();
         ib.Unbind();
 
-        float r = 0.0f, inc = 0.05f;
-
         GLClearError();
 
-        /* Loop until the user closes the window */
+        ImGui::CreateContext();
+        ImGui_ImplGlfwGL3_Init(window, true);
+        ImGui::StyleColorsDark();
+
+
+        Renderer renderer;
         while (!glfwWindowShouldClose(window))
         {
-            /* Render here */
-            Clear();
+            //Timer timer;
+            renderer.clear();
 
-            va.Bind();
-            ib.Bind();
-            shader.Bind();
-            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+            ImGui_ImplGlfwGL3_NewFrame();
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+                glm::mat4 mvp = proj * view * model;
+
+                shader.SetUniformMat4f("u_MVP", mvp);
+                renderer.draw(va, ib, shader);
+            }            
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+                glm::mat4 mvp = proj * view * model;
+
+                shader.SetUniformMat4f("u_MVP", mvp);
+                renderer.draw(va, ib, shader);
+            }
             //glDrawArrays(GL_POLYGON, 0, 4);
-            /* Swap front and back buffers */
-            glfwSwapBuffers(window);
+            {
+                ImGui::SliderFloat3("translation A: ", &translationA.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+                ImGui::SliderFloat3("translation B: ", &translationB.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            }
 
-            /* Poll for and process events */
+            ImGui::Render();
+            ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+            glfwSwapBuffers(window);
             glfwPollEvents();
 
         }
     }
+    ImGui_ImplGlfwGL3_Shutdown();
+    ImGui::DestroyContext();
     glfwDestroyWindow(window);
     glfwTerminate();
 
--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -20,11 +20,53 @@ void Clear()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 }
-/*
+
 void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader)
 {
     shader.Bind();
     va.Bind();
     ib.Bind();
     glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr);
-}*/
+}
+
+void UnbindAll()
+{
+    glUseProgram(0);
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+Renderer::Renderer()
+{
+
+}
+
+Renderer::~Renderer()
+{
+    glUseProgram(0);
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void Renderer::draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader)
+{
+    shader.Bind();
+    va.Bind();
+    ib.Bind();
+    glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr);
+}
+
+void Renderer::clear()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Renderer::unbindall()
+{
+    glUseProgram(0);
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
